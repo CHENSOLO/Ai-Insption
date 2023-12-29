1c821bcefd807eb297e983e755b07657202f24ef
CHENSOLO <lmxi_dei@qq.com>
2023-12-29 18:30:00 +0800

    初始化

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,83 @@
+// pages/welcome/welcome.js
+
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  //获取头像
+  onChooseAvatar(event){
+    const {avatarUrl} = event.detail
+    this.setData({
+      avatarUrl
+    })
+  },
+
+  //跳转到首页
+  onTap(event){
+    wx.navigateTo({
+      url: '../Ai-tools/Ai-tools',  
+  })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
