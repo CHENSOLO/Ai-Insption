2d75604b922546d7e406465e20526ebfb421e730
CHENSOLO <lmxi_dei@qq.com>
2024-01-02 18:26:51 +0800

    首页布局编写

--- pages/Ai-tools/Ai-tools.js
+++ pages/Ai-tools/Ai-tools.js
@@ -14,14 +14,16 @@ Page({
   onLoad(options) {
 
   },
-
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
 
   },
-
+  changeTabs(event){
+    console.log(event)
+  },
   /**
    * 生命周期函数--监听页面显示
    */
